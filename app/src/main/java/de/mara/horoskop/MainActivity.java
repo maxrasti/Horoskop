65fe4e8f0e8a83ac782e29ef2ed6a0233265e3e3
Max Rastetter <mrastetter@united.domain>
2018-03-08 22:42:34 +0100

    test 1

--- app/src/main/java/de/mara/horoskop/MainActivity.java
+++ app/src/main/java/de/mara/horoskop/MainActivity.java
@@ -7,6 +7,13 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     protected Button testbutton=null;
@@ -22,11 +29,54 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         testbutton.setOnClickListener(this);
     }
 
-    public void onClick (View v){
+    public void onClick (View v) {
 
-        Toast toast = Toast.makeText(this,"string", Toast.LENGTH_LONG);
+        Toast toast = Toast.makeText(this, "string", Toast.LENGTH_LONG);
 
         toast.show();
 
+        URL url = null;
+
+        HttpURLConnection client = null;
+
+        String sign = "aries";
+        String day = "day";
+
+        try {
+            url = new URL("https://aztro.herokuapp.com?sign="+sign+"&day="+day);
+            client = (HttpURLConnection) url.openConnection();
+            client.setRequestMethod("POST");
+           // client.setRequestProperty();
+            client.setDoOutput(true);
+
+            OutputStream outputPost = new BufferedOutputStream(client.getOutputStream());
+
+
+            //writeStream(outputPost);
+
+
+            Toast toast2 = Toast.makeText(this,outputPost.toString(), Toast.LENGTH_LONG);
+
+            toast2.show();
+
+            outputPost.flush();
+            outputPost.close();
+
+
+
+        } catch (IOException e){
+
+        }
+
+        finally {
+            if(client != null) // Stelle sicher, dass die Verbindung nicht null ist.
+                client.disconnect();
+        }
+
+
+
+
     }
-}
+
+    }
+
