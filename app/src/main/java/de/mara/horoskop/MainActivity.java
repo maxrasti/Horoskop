2389bf6ca18a0ce1f67a5c3505829b8ce2f7b48e
Max Rastetter <mrastetter@united.domain>
2018-03-08 21:12:54 +0100

    Test Button mit Listener und Toast

--- app/src/main/java/de/mara/horoskop/MainActivity.java
+++ app/src/main/java/de/mara/horoskop/MainActivity.java
@@ -2,12 +2,31 @@ package de.mara.horoskop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    protected Button testbutton=null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        testbutton=(Button) findViewById(R.id.button);
+
+        testbutton.setOnClickListener(this);
+    }
+
+    public void onClick (View v){
+
+        Toast toast = Toast.makeText(this,"", Toast.LENGTH_LONG);
+
+        toast.show();
+
     }
 }
